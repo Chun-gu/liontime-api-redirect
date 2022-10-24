fb7b1258fc64334be8bd5855b32748ad2c266ebb
Chun-gu <chungu.yi92@gmail.com>
2022-10-25 04:04:28 +0900

    ✨ serverless function 구현

---
+++ functions/index.js
@@ -0,0 +1,43 @@
+import https from "https";
+import fetch from "node-fetch";
+
+export async function handler(event) {
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+  });
+
+  const headers = {
+    "Access-Control-Allow-Origin": process.env.HOST,
+    "Content-Type": "application/json",
+  };
+
+  const {
+    httpMethod,
+    headers: { referer },
+    body,
+  } = event;
+
+  const path = new URL(referer).pathname;
+
+  let response;
+  try {
+    response = await fetch(process.env.API_URL + path, {
+      headers,
+      method: httpMethod,
+      agent: httpsAgent,
+      body,
+    }).then((res) => res.json());
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message,
+      }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(response),
+  };
+}
