ca34985e05692ff0f67dfa7b6da13863e489df2a
Chun-gu <chungu.yi92@gmail.com>
2022-11-23 14:28:40 +0900

    🧪 File 사용 함수 주석처리 테스트

--- functions/image.js
+++ functions/image.js
@@ -1,62 +1,62 @@
-import https from "https";
-import fetch, { File, FormData } from "node-fetch";
-import { parseMultipartForm } from "./parseMultipartForm";
+// import https from "https";
+// import fetch, { File, FormData } from "node-fetch";
+// import { parseMultipartForm } from "./parseMultipartForm";
 
-export async function handler(event) {
-  const { path, httpMethod } = event;
+// export async function handler(event) {
+//   const { path, httpMethod } = event;
 
-  if (httpMethod === "OPTIONS")
-    return {
-      statusCode: 200,
-      ok: true,
-      headers: {
-        "Access-Control-Allow-Origin": process.env.HOST,
-        "Access-Control-Allow-Headers": "*",
-        "Access-Control-Allow-Methods": "POST",
-      },
-    };
+//   if (httpMethod === "OPTIONS")
+//     return {
+//       statusCode: 200,
+//       ok: true,
+//       headers: {
+//         "Access-Control-Allow-Origin": process.env.HOST,
+//         "Access-Control-Allow-Headers": "*",
+//         "Access-Control-Allow-Methods": "POST",
+//       },
+//     };
 
-  const httpsAgent = new https.Agent({
-    rejectUnauthorized: false,
-  });
+//   const httpsAgent = new https.Agent({
+//     rejectUnauthorized: false,
+//   });
 
-  const { image } = await parseMultipartForm(event);
+//   const { image } = await parseMultipartForm(event);
 
-  const formData = new FormData();
-  for (const img of image) {
-    const file = new File([img.content], img.filename, {
-      type: img.type,
-    });
-    formData.append("image", file);
-  }
+//   const formData = new FormData();
+//   for (const img of image) {
+//     const file = new File([img.content], img.filename, {
+//       type: img.type,
+//     });
+//     formData.append("image", file);
+//   }
 
-  try {
-    const response = await fetch(process.env.API_URL + path, {
-      agent: httpsAgent,
-      method: httpMethod,
-      body: formData,
-    });
+//   try {
+//     const response = await fetch(process.env.API_URL + path, {
+//       agent: httpsAgent,
+//       method: httpMethod,
+//       body: formData,
+//     });
 
-    const { status, ok, headers } = response;
-    const resJson = await response.json();
-    const body = JSON.stringify(resJson);
+//     const { status, ok, headers } = response;
+//     const resJson = await response.json();
+//     const body = JSON.stringify(resJson);
 
-    headers["Access-Control-Allow-Origin"] = "*";
+//     headers["Access-Control-Allow-Origin"] = "*";
 
-    return {
-      statusCode: status,
-      ok,
-      headers,
-      body,
-    };
-  } catch (err) {
-    return {
-      statusCode: 404,
-      statusText: err.message,
-      ok: false,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-    };
-  }
-}
+//     return {
+//       statusCode: status,
+//       ok,
+//       headers,
+//       body,
+//     };
+//   } catch (err) {
+//     return {
+//       statusCode: 404,
+//       statusText: err.message,
+//       ok: false,
+//       headers: {
+//         "Access-Control-Allow-Origin": "*",
+//       },
+//     };
+//   }
+// }
--- functions/parseMultipartForm.js
+++ functions/parseMultipartForm.js
@@ -1,33 +1,33 @@
-import busboy from "busboy";
+// import busboy from "busboy";
 
-export function parseMultipartForm(event) {
-  return new Promise((resolve) => {
-    const fields = {};
+// export function parseMultipartForm(event) {
+//   return new Promise((resolve) => {
+//     const fields = {};
 
-    const busBoy = busboy({
-      headers: event.headers,
-    });
+//     const busBoy = busboy({
+//       headers: event.headers,
+//     });
 
-    const files = [];
-    busBoy.on("file", (fieldname, filestream, filename) => {
-      filestream.on("data", (data) => {
-        files.push({
-          content: data,
-          filename: filename.filename,
-          type: filename.mimeType,
-        });
-        fields[fieldname] = files;
-      });
-    });
+//     const files = [];
+//     busBoy.on("file", (fieldname, filestream, filename) => {
+//       filestream.on("data", (data) => {
+//         files.push({
+//           content: data,
+//           filename: filename.filename,
+//           type: filename.mimeType,
+//         });
+//         fields[fieldname] = files;
+//       });
+//     });
 
-    busBoy.on("field", (fieldName, value) => {
-      fields[fieldName] = value;
-    });
+//     busBoy.on("field", (fieldName, value) => {
+//       fields[fieldName] = value;
+//     });
 
-    busBoy.on("close", () => {
-      resolve(fields);
-    });
+//     busBoy.on("close", () => {
+//       resolve(fields);
+//     });
 
-    busBoy.end(Buffer.from(event.body, "base64"));
-  });
-}
+//     busBoy.end(Buffer.from(event.body, "base64"));
+//   });
+// }
